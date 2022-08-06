128158126922aa4dcc541552c0c686c848c38362
Tatsiana A <astrouskaya@yahoo.com>
2022-08-06 12:35:47 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// console.log('---js----');
+//
+// const titles = document.getElementsByTagName('h2')
+// for(let i = 0; i < titles.length; i++) {
+//     console.log(titles[i]);
+// }
+// console.log([...titles]); // modify collection into array
+// const arr = Array.from(titles); // modify collection into array
+// console.log(arr);
+//
+// let i = 5;
+//
+// function plusOne() {
+//     console.log(++i);
+// }
+//
+// function minusOne() {
+//     console.log(--i);
+// }
+
+// let idOne = document.getElementById('one');
+// console.log(idOne);
+// console.log(idOne.innerText);
+// console.log(idOne.innerHTML);
+
+let subTitles = document.getElementsByClassName('sub-title');
+console.log(subTitles);
